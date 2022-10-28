0a78bd40cc6e5a205fffb44536a15144b5439bbe
donghyun9131 <dong60947@gmail.com>
2022-10-28 13:34:03 +0900

    회원 가입시, 입력데이터 유효성과 공백 체크

--- src/main/java/com/sbs/exam/sb_app_2022_10_13/member/controller/UsrMemberController.java
+++ src/main/java/com/sbs/exam/sb_app_2022_10_13/member/controller/UsrMemberController.java
@@ -20,27 +20,27 @@ public class UsrMemberController {
   public Object doJoin(String loginId, String loginPw, String name, String nickname, String cellphoneNo, String email) {
     int id = memberService.join(loginId, loginPw, name, nickname, cellphoneNo, email);
 
-    if ( loginId == null ) {
+    if ( loginId == null || loginId.trim().length() == 0 ) {
       return "loginId(을)를 입력 해주세요.";
     }
 
-    if( loginPw == null ) {
+    if( loginPw == null  || loginPw.trim().length() == 0 ) {
       return "loginPw(을)를 입력 해주세요.";
     }
 
-    if( name == null ) {
+    if( name == null || name.trim().length() == 0 ) {
       return "name(을)를 입력 해주세요.";
     }
 
-    if( nickname == null ) {
+    if( nickname == null || nickname.trim().length() == 0 ) {
       return "nickname(을)를 입력 해주세요.";
     }
 
-    if( cellphoneNo == null ) {
+    if( cellphoneNo == null || cellphoneNo.trim().length() == 0 ) {
       return "cellphoneNo(을)를 입력 해주세요.";
     }
 
-    if( email == null ) {
+    if( email == null || email.trim().length() == 0 ) {
       return "email(을)를 입력 해주세요.";
     }
 
