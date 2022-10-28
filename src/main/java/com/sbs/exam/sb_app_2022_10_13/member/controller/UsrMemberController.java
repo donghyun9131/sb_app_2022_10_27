a1d4e8bd44449e05a0ac51079838df65edd69d91
donghyun9131 <dong60947@gmail.com>
2022-10-28 13:26:59 +0900

    회원 가입 시, 입력 데이터 유효성 체크

--- src/main/java/com/sbs/exam/sb_app_2022_10_13/member/controller/UsrMemberController.java
+++ src/main/java/com/sbs/exam/sb_app_2022_10_13/member/controller/UsrMemberController.java
@@ -20,6 +20,30 @@ public class UsrMemberController {
   public Object doJoin(String loginId, String loginPw, String name, String nickname, String cellphoneNo, String email) {
     int id = memberService.join(loginId, loginPw, name, nickname, cellphoneNo, email);
 
+    if ( loginId == null ) {
+      return "loginId(을)를 입력 해주세요.";
+    }
+
+    if( loginPw == null ) {
+      return "loginPw(을)를 입력 해주세요.";
+    }
+
+    if( name == null ) {
+      return "name(을)를 입력 해주세요.";
+    }
+
+    if( nickname == null ) {
+      return "nickname(을)를 입력 해주세요.";
+    }
+
+    if( cellphoneNo == null ) {
+      return "cellphoneNo(을)를 입력 해주세요.";
+    }
+
+    if( email == null ) {
+      return "email(을)를 입력 해주세요.";
+    }
+
     if ( id == -1) {
       return "해당 로그인아이디는 이미 사용중입니다.";
     }
