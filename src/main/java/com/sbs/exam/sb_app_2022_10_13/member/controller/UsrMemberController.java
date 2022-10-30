3de81da0c6a156b84e4effb556dfdeb4c9b9a00f
donghyun9131 <dong60947@gmail.com>
2022-10-30 20:34:03 +0900

    Ut.f 함수 도입으로 복잡한 문장 구성을 편하게

--- src/main/java/com/sbs/exam/sb_app_2022_10_13/member/controller/UsrMemberController.java
+++ src/main/java/com/sbs/exam/sb_app_2022_10_13/member/controller/UsrMemberController.java
@@ -46,11 +46,11 @@ public class UsrMemberController {
     }
 
     if ( id == -1) {
-      return "해당 로그인아이디는 이미 사용중입니다.";
+      return Ut.f("해당 로그인아이디(%s)는 이미 사용중입니다.", loginId);
     }
 
     if ( id == -2 ) {
-      return "해당 이름과 이메일은 이미 사용중입니다.";
+      return Ut.f("해당 이름(%s)과 이메일(%s)은 이미 사용중입니다.", name, email);
     }
 
     Member member = memberService.getMemberById(id);
--- src/main/java/com/sbs/exam/sb_app_2022_10_13/util/Ut.java
+++ src/main/java/com/sbs/exam/sb_app_2022_10_13/util/Ut.java
@@ -14,4 +14,8 @@ public class Ut {
 
     return str.trim().length() == 0;
   }
+
+  public static Object f(String format, Object... args) {
+    return String.format(format, args);
+  }
 }
