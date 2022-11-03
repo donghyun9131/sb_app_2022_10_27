4cbf515068d3fe1b1f003e1e4c805e4123cb24a9
donghyun9131 <dong60947@gmail.com>
2022-11-03 20:38:01 +0900

    게시물 삭제시 권한체크

--- db/schema.sql
+++ db/schema.sql
@@ -88,3 +88,8 @@ SELECT * FROM `member`;
 
 ALTER TABLE article ADD COLUMN memberId INT(10) UNSIGNED NOT NULL AFTER `updateDate`;
 
+# 기존 게시물의 작성자를 2번으로 지정
+UPDATE article
+SET memberId = 2
+WHERE memberId = 0;
+
--- src/main/java/com/sbs/exam/sb_app_2022_10_13/controller/UsrArticleController.java
+++ src/main/java/com/sbs/exam/sb_app_2022_10_13/controller/UsrArticleController.java
@@ -73,8 +73,24 @@ public class UsrArticleController {
 
   @RequestMapping("/usr/article/doDelete")
   @ResponseBody
-  public ResultData<Integer> doDelete(int id) {
+  public ResultData<Integer> doDelete(HttpSession httpSession, int id) {
+    boolean isLogined = false;
+    int loginedMemberId = 0;
+
+    if (httpSession.getAttribute("loginedMemberId") != null ) {
+      isLogined = true;
+      loginedMemberId = (int) httpSession.getAttribute("loginedMemberId");
+    }
+
+    if ( isLogined == false ) {
+      return ResultData.from("F-A", "로그인 후 이용해주세요.");
+    }
+
     Article article = articleService.getArticle(id);
+
+    if (article.getMemberId() != loginedMemberId) {
+      return ResultData.from("F-2", "권한이 없습니다.");
+    }
 
     if ( article == null ) {
       return ResultData.from("F-1", Ut.f("%d번 게시물이 존재하지 않습니다.", id));
