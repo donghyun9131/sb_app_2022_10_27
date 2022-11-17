763e4b918f041186b58612510fc2f77ca3f504dd
donghyun9131 <dong60947@gmail.com>
2022-11-17 14:33:35 +0900

    게시물 수정 후에, 자바스크립트로 후처리

--- src/main/java/com/sbs/exam/sb_app_2022_10_27/controller/UsrArticleController.java
+++ src/main/java/com/sbs/exam/sb_app_2022_10_27/controller/UsrArticleController.java
@@ -116,30 +116,31 @@ public class UsrArticleController {
 
   @RequestMapping("/usr/article/doModify")
   @ResponseBody
-  public ResultData<Article> doModify(HttpServletRequest req, int id, String title, String body) {
+  public String doModify(HttpServletRequest req, int id, String title, String body) {
     Rq rq = (Rq) req.getAttribute("rq");
 
     if ( rq.isLogined() == false ) {
-      return ResultData.from("F-A", "로그인 후 이용해주세요.");
+      return Ut.jsHistoryBack("로그인 후 이용해주세요.");
     }
 
     Article article = articleService.getForPrintArticle(rq.getLoginedMemberId(), id);
 
     if(article.getMemberId() != rq.getLoginedMemberId() ) {
-      return ResultData.from("F-2", "권한이 없습니다.");
+      return Ut.jsHistoryBack("권한이 없습니다.");
     }
 
 
     if ( article == null ) {
-      return ResultData.from("F-1", Ut.f("%d번 게시물이 존재하지 않습니다.", id));
+      return Ut.jsHistoryBack(Ut.f("%d번 게시물이 존재하지 않습니다.", id));
     }
 
     ResultData actorCanModifyRd = articleService.actorCanModify(rq.getLoginedMemberId(), article);
 
     if (actorCanModifyRd.isFail()) {
-      return actorCanModifyRd;
+      return Ut.jsHistoryBack(actorCanModifyRd.getMsg());
     }
 
-    return articleService.modifyArticle(id, title, body);
+    articleService.modifyArticle(id, title, body);
+    return Ut.jsReplace(Ut.f("%d번 게시물이 수정 되었습니다.", id), Ut.f("../article/detail?id=%d", id));
   }
 }
