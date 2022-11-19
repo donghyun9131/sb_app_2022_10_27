3b1f03c16cb107bb2b050be09629c0278112530e
donghyun9131 <dong60947@gmail.com>
2022-11-19 20:04:47 +0900

    멀티게시판 개념 도입, board 테이블 생성

--- db/schema.sql
+++ db/schema.sql
@@ -92,3 +92,44 @@ ALTER TABLE article ADD COLUMN memberId INT(10) UNSIGNED NOT NULL AFTER `updateD
 UPDATE article
 SET memberId = 2
 WHERE memberId = 0;
+
+# 게시판 테이블 생성
+CREATE TABLE board (
+	id INT(10) UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	regDate DATETIME NOT NULL,
+	updateDate DATETIME NOT NULL,
+	`code` CHAR(50) NOT NULL UNIQUE COMMENT 'notice=공지사항,free1=자유게시판,free2=자유게시판2...',
+	`name` CHAR(50) NOT NULL UNIQUE COMMENT '게시판 이름',
+	delStatus TINYINT(1) UNSIGNED NOT NULL DEFAULT 0 COMMENT '삭제여부(0=삭제전, 1=삭제)',
+	delDate DATETIME COMMENT '삭제날짜'
+);
+
+# 기본 게시판 생성
+
+INSERT INTO board
+SET regDate = NOW(),
+updateDate = NOW(),
+`code` = 'notice',
+`name` = '공지사항';
+
+INSERT INTO board
+SET regDate = NOW(),
+updateDate = NOW(),
+`code` = 'free1',
+`name` = '자유게시판';
+
+# 게시판 테이블 boardId 칼럼 추가
+
+ALTER TABLE article ADD COLUMN boardId INT(10) UNSIGNED NOT NULL AFTER memberId;
+
+# 1, 2번 게시물을 공지사항 게시물로 지정
+
+UPDATE article
+SET boardId = 1
+WHERE id IN(1, 2);
+
+# 3번 게시물을 자유게시판 게시물로 지정
+UPDATE article
+SET boardId = 2
+WHERE id IN(3);
+
