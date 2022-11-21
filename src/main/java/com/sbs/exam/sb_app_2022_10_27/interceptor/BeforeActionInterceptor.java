598cf93919d408106cf8d5ef7cbf116968158528
donghyun9131 <dong60947@gmail.com>
2022-11-21 11:48:19 +0900

    BeforeActionInterceptor 에서 Rq 객체를 좀 더 확실하게 생성되도록 유도

--- src/main/java/com/sbs/exam/sb_app_2022_10_27/interceptor/BeforeActionInterceptor.java
+++ src/main/java/com/sbs/exam/sb_app_2022_10_27/interceptor/BeforeActionInterceptor.java
@@ -1,5 +1,6 @@
 package com.sbs.exam.sb_app_2022_10_27.interceptor;
 
+import com.sbs.exam.sb_app_2022_10_27.vo.Rq;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 
@@ -8,10 +9,17 @@ import javax.servlet.http.HttpServletResponse;
 
 @Component
 public class BeforeActionInterceptor implements HandlerInterceptor {
+  private Rq rq;
+
+  public BeforeActionInterceptor(Rq rq) {
+    this.rq = rq;
+  }
+
+
 
   @Override
   public boolean preHandle(HttpServletRequest req, HttpServletResponse resp, Object handle) throws Exception {
-    // 이제는 Rq 객체가 자동으로 만들어지기 때문에 필요 없음
+    rq.initOnBeforeActionInterceptor();
 
     return HandlerInterceptor.super.preHandle(req, resp, handle);
   }
--- src/main/java/com/sbs/exam/sb_app_2022_10_27/vo/Rq.java
+++ src/main/java/com/sbs/exam/sb_app_2022_10_27/vo/Rq.java
@@ -89,4 +89,11 @@ public class Rq {
   public String jsReplace(String msg, String uri) {
     return Ut.jsReplace(msg, uri);
   }
+
+  // 이 메서드는 Rq 객체가 자연스럽게 생성되도록 유도하는 역할을 한다.
+  // 지우면 안되고,
+  // 편의를 위해 beforeActionInterceptor 에서 꼭 호출을 해줘야 한다.
+  public void initOnBeforeActionInterceptor() {
+
+  }
 }
